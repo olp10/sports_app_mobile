d0f23add8b4ec349e41a70b368deaf2baa98cde5
Oli <olp10@hi.is>
2023-02-04 14:18:54 +0000

    initial commit

---
+++ app/src/main/java/com/example/sports_app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sports_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
