912193a49fd611c029005af093d5dc64c2868281
Oli <olp10@hi.is>
2023-02-18 13:16:48 +0000

    Vinna í login

--- app/src/main/java/com/example/sports_app/MainActivity.java
+++ app/src/main/java/com/example/sports_app/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.sports_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ActionMenuView;
+
 import com.example.sports_app.entities.Thread;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private List<Thread> threads;
+    ActionMenuView mActionMenuView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mActionMenuView = (ActionMenuView) findViewById(R.id.toolbar_bottom);
 
         /*
         NetworkManager networkManager = NetworkManager.getInstance(this);
@@ -35,4 +46,23 @@ public class MainActivity extends AppCompatActivity {
         */
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_actionview, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_login:
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void login() {
+
+    }
 }
