35eb658a7000b80e2824ffcc76d7365966b23bdd
Oli <olp10@hi.is>
2023-02-19 11:47:05 +0000

    Meiri vinna í login

--- app/src/main/java/com/example/sports_app/SportActivity.java
+++ app/src/main/java/com/example/sports_app/SportActivity.java
@@ -1,11 +1,14 @@
 package com.example.sports_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ActionMenuView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,12 +19,10 @@ public class SportActivity extends AppCompatActivity {
     ActionMenuView mActionMenuView;
     private static final String TAG = "MainActivity";
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sport);
-
+    public void InstantiateUIElements() {
         mActionMenuView = (ActionMenuView) findViewById(R.id.toolbar_bottom);
+
+        // Adding tabs (Threads/Events/Clubs) for sport
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
 
         TabLayout.Tab threadsTab = tabLayout.newTab();
@@ -37,9 +38,29 @@ public class SportActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sport);
+        InstantiateUIElements();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_actionview, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_login:
+                startActivity(new Intent(SportActivity.this, LoginActivity.class));
+                break;
+            case R.id.menu_home:
+                startActivity(new Intent(SportActivity.this, MainActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
